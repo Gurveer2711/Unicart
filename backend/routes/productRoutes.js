16c7392abc93437b19f9f2b9369c06770c534c07
Gurveer27 <singhgurveer337@gmail.com>
2025-05-02 02:40:10 +0530

    corrected the routes order

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,13 +2,15 @@ import express from "express";
 import {
   getProducts,
   getProductById,
+  getTopSellingProducts,
+  getNewProducts,
 } from "../controllers/productController.js";
 
 const router = express.Router();
 
+router.get("/top-selling", getTopSellingProducts);
+router.get("/new", getNewProducts);
 router.get("/", getProducts);
 router.get("/:id", getProductById);
-router.get("/top-selling", getTopSellingProducts);
-router.get("/new", getNewProductsByCategory);
 
 export default router;
