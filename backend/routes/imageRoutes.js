5bc9a75655c5f8be6bdf8935a51a8dde1598c977
Gurveer27 <singhgurveer337@gmail.com>
2025-06-19 01:48:07 +0530

    refactor: remove upload middleware from imageRoutes; simplify image upload handling

--- backend/routes/imageRoutes.js
+++ backend/routes/imageRoutes.js
@@ -1,15 +1,14 @@
 import express from "express";
-import upload from "../middleware/uploadMiddleware.js";
 import { removeImage, saveImage, uploadImage } from "../controllers/imageController.js";
 
 
 const router = express.Router();
 
 // Upload image to local 'uploads/' folder
-router.post("/upload", upload.single("image"),uploadImage);
+router.post("/upload", uploadImage);
 
 // Upload image to Cloudinary
-router.post("/cloudinary", upload.single("image"),saveImage);
+router.post("/cloudinary",saveImage);
 
 // Remove local file
 router.post("/remove",removeImage);
